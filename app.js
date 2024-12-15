0dc28748f3b26794ec1a1ebe6fa442721f91b369
simplyObare <josephatowiny@gmail.com>
2024-12-15 23:50:28 +0300

    project complete

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ heroBtn.addEventListener('mouseover', () => {
   heroBtn.classList.add(
     'text-BrownTumbleweed',
     'bg-MacaroniCheese',
-    'transform',
-    'scale-110'
+    'transform'
   )
   btnCart.classList.add('bg-BrownTumbleweed')
   btnCart.classList.remove('bg-MacaroniCheese')
@@ -16,9 +15,34 @@ heroBtn.addEventListener('mouseout', () => {
   heroBtn.classList.remove(
     'text-BrownTumbleweed',
     'bg-MacaroniCheese',
-    'transform',
-    'scale-110'
+    'transform'
   )
   btnCart.classList.add('bg-MacaroniCheese')
   btnCart.classList.remove('bg-BrownTumbleweed')
+})
+
+const hamburgerContainer = document.querySelector('.hamburger-container')
+const mobileBar = document.querySelector('.mobile-bar')
+const mobileBarBtn = document.querySelector('.mobile-bar-btn')
+let barStatus = false
+
+hamburgerContainer.addEventListener('click', () => {
+  barStatus = !barStatus
+  if (barStatus) {
+    mobileBar.classList.add(
+      'block',
+      'z-50',
+      'transform',
+      'transition-transform',
+      'duration-300',
+      'translate-x-0'
+    )
+    mobileBar.classList.remove('-translate-x-full', 'hidden')
+  }
+})
+
+mobileBarBtn.addEventListener('click', () => {
+  mobileBar.classList.remove('z-50', 'block')
+  mobileBar.classList.add('-translate-x-full', 'hidden')
+  mobileBar.classList.remove('translate-x-0')
 })
